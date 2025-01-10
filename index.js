f34b8a30a8a922839d825bb5f2d1ea84cb1a5045
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2025-01-10 13:48:54 -0700

    looping

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+const names = ["Sebastian", "Mateo", "Ela"]
+function writeCards(names){
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+}return messages;}
+function countDown(number = 10 ) {
+    for (let i = 10; i >= 0; i--){
+        console.log(i);
+    }
+}
